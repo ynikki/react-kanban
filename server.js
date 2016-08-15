d8e4db5ee6d45367e1fa74a6424de9ab6558bcad
Nikki K <808nikz@gmail.com>
2016-08-14 21:53:01 -1000

    Added method-override

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const pug = require('pug');
+const methodOverride = require('method-override');
 
 
 app.set('port', (process.env.PORT || 8080));
@@ -15,6 +16,14 @@ app.set('view engine', 'pug');
 app.use('/', express.static(path.join(__dirname, 'views/index')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extneded: false }));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 
 app.get('/', function (req, res) {
  res.render('index');
