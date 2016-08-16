614ecc36433a9c6f4ba9a86d783242f115ac1a1b
Nikki K <808nikz@gmail.com>
2016-08-16 12:26:49 -1000

    Added Text next to the input

--- public/app.js
+++ public/app.js
@@ -1,26 +1,82 @@
 const TaskForm = React.createClass({
+  getInitialState: function () {
+    return {
+      title: '',
+      description: '',
+      createdBy: '',
+      assignedTo: '',
+      priority: ''
+    };
+  },
+  handleTitleChange: function (e) {
+    this.setState({ title: e.target.value });
+  },
+  handleDescriptionChange: function (e) {
+    this.setState({ description: e.target.value });
+  },
+  handleCreatedByChange: function (e) {
+    this.setState({ createdBy: e.target.value });
+  },
+  handleAssignedToChange: function (e) {
+    this.setState({ assignedTo: e.target.value });
+  },
+  handlePriorityChange: function (e) {
+    this.setState({ priority: e.target.value });
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+
+    let title = this.state.title.trim();
+    let description = this.state.description.trim();
+    let createdBy = this.state.createdBy.trim();
+    let assignedTo = this.state.assignedTo.trim();
+    let priority = this.state.priority.trim();
+
+    if(!title || !description || !createdBy || !assignedTo || !priority) {
+      return;
+    }
+    this.props.onCommentSubmit({
+      title: title,
+      description: description,
+      createdBy: createdBy,
+      assignedTo: assignedTo,
+      priority: priority
+    });
+    this.setState({
+      title: '',
+      description: '',
+      createdBy: '',
+      assignedTo: '',
+      priority: ''
+    });
+  },
   render: function () {
     return (
       <form className="taskForm" onSubmit= { this.handleSubmit }>
+        Title: 
         <input
           type="text"
           placeholder="Title"
         />
+        Description: 
         <input
           type="text"
           placeholder="Description"
         />
+        Created By: 
         <input
           type="text"
           placeholder="Created By"
         />
+        Assigned To: 
         <input
           type="text"
           placeholder="Assigned To"
         />
+        Priority
         <input
           type="text"
-          placeholder="Created By"
+          placeholder="Priority"
         />
         <input
           type="submit"
