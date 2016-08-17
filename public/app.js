873bcd8b1c44dd584e2973c0d20061308f651f99
Nikki K <808nikz@gmail.com>
2016-08-16 21:45:14 -1000

    Started adding more to it

--- public/app.js
+++ public/app.js
@@ -32,7 +32,7 @@ const TaskForm = React.createClass({
     let assignedTo = this.state.assignedTo.trim();
     let priority = this.state.priority.trim();
 
-    if(!title || !description || !createdBy || !assignedTo || !priority) {
+    if ( !title || !description || !createdBy || !assignedTo || !priority ) {
       return;
     }
     this.props.onCommentSubmit({
@@ -57,26 +57,36 @@ const TaskForm = React.createClass({
         <input
           type="text"
           placeholder="Title"
+          value={ this.state.title }
+          onChange={ this.handleTitleChange }
         />
         Description: 
         <input
           type="text"
           placeholder="Description"
+          value={ this.state.description }
+          onChange={ this.handleDescriptionChange }
         />
         Created By: 
         <input
           type="text"
           placeholder="Created By"
+          value={ this.state.createdBy }
+          onChange={ this.handleCreatedByChange }
         />
         Assigned To: 
         <input
           type="text"
           placeholder="Assigned To"
+          value={ this.state.assignedTo }
+          onChange={ this.handleAssignedToChange }
         />
         Priority
         <input
           type="text"
           placeholder="Priority"
+          value={ this.state.createdBy }
+          onChange={ this.handlePriorityChange }
         />
         <input
           type="submit"
@@ -88,12 +98,20 @@ const TaskForm = React.createClass({
 });
 
 const Task = React.createClass({
+  rawMarkup: function () {
+    const md = new Remarkable();
+    const rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
   render: function () {
     return (
       <div className="title">
         <h2 className="taskTitle">
-          Title Goes Here
+          { this.props.title }
         </h2>
+        <span
+          dangerouslySetInnerHTML={ this.rawMarkup() }
+        />
       </div>
     );
   }
@@ -101,26 +119,70 @@ const Task = React.createClass({
 
 const TaskList = React.createClass({
   render: function () {
+    const taskNodes = this.props.data.map(function (task, index) {
+      return (
+        <Comment
+          key={ index }
+          title={ task.title }
+        >
+          { task.text }
+        </Comment>
+      );
+    });
     return (
       <div className="taskList">
-      THE LIST
+        { taskNodes.reverse() }
       </div>
     );
   }
 });
 
 const TaskBox = React.createClass({
+  loadTasksFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleTaskSubmit: function (task) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: task,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function () {
+    return { data: [] }
+  },
+  componentDidMount: function () {
+    this.loadTasksFromServer();
+    setInterval(this.loadTasksFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="taskBox">
-        <TaskForm />
-        <TaskList />
+        <TaskForm data={ this.state.data } />
+        <TaskList onTaskSubmit={ this.handleTaskSubmit }/>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <TaskBox url="/task/new" />,
+  <TaskBox url="/task/new" pollInterval={2000} />,
   document.getElementById('app')
 );
