1f102608c1c0513749ccb57019f8282b90369c1a
Nikki K <808nikz@gmail.com>
2016-08-18 21:03:15 -1000

    Added test status

--- seeders/20160814054026-status.js
+++ seeders/20160814054026-status.js
@@ -7,6 +7,21 @@ module.exports = {
       "name": "URGENT",
       createdAt: new Date(),
       updatedAt: new Date()
+    },
+    {
+      "name": "LOW",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      "name": "MEDIUM",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      "name": "BLOCKER",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }],{
     })
   },
