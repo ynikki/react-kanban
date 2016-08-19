e3f597eb59fda17d06e484d11383341c9da6d1b9
Nikki K <808nikz@gmail.com>
2016-08-18 21:01:09 -1000

    created a statusId for the Task Table

---
+++ migrations/20160819063614-statusId.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface
+      .addColumn('Tasks', 'status_id', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        foreignKey: true,
+        references: {
+          model: 'Tasks',
+          key: 'id'
+        }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface
+      .removeColumn('Tasks', 'status_id');
+  }
+};
