ef8f4835f9903daf2d5b3ebc1a1aa30ddbc0d117
Nikki K <808nikz@gmail.com>
2016-08-19 21:53:44 -1000

    Created a models table for the userTask

---
+++ models/usertask.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var UserTask = sequelize.define('UserTask', {
+    user_id: DataTypes.INTEGER,
+    status_id: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return UserTask;
+};
