8b38358cce3a2c1b79fc796b25708acd2ec1e1e9
Nikki K <808nikz@gmail.com>
2016-08-19 21:52:32 -1000

    Added the foreign key for user_id to the user models

--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.User.hasMany(models.Task, {
+          foreignKey: 'user_id'
+        });
       }
     }
   });
