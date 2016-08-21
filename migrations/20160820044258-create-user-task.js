6b70d5d5ff917b91080de915ab22131105413cc6
Nikki K <808nikz@gmail.com>
2016-08-20 14:32:09 -1000

    Migrations for the user task table

--- migrations/20160820044258-create-user-task.js
+++ migrations/20160820044258-create-user-task.js
@@ -2,26 +2,7 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.createTable('UserTasks', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      user_id: {
-        type: Sequelize.INTEGER
-      },
-      status_id: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+      
     });
   },
   down: function(queryInterface, Sequelize) {
