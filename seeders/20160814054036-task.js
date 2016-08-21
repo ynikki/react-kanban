5e7832a7a7cf7b6aed5c40dbd85638fb4ef5a4fb
Nikki K <808nikz@gmail.com>
2016-08-20 14:31:48 -1000

    Added test task

--- seeders/20160814054036-task.js
+++ seeders/20160814054036-task.js
@@ -6,11 +6,31 @@ module.exports = {
     {
       "title": "KANBAN",
       "description": "Finish this on time...",
-      "createdBy": "blank_something",
+      "user_id": 1,
+      "status_id": 2,
       "assignedTo": "something",
       "priority": "4",
       createdAt: new Date(),
-      updatedAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      "title": "This is a Test",
+      "description": "This is another test...",
+      "user_id": 2,
+      "status_id": 3,
+      "assignedTo": "noo",
+      "priority": "2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      "title": "Another One Bites The Dust",
+      "description": "Puhleeeez work, pretty please.",
+      "user_id": 3,
+      "assignedTo": "youu",
+      "priority": "5",
+      createdAt: new Date(),
+      updatedAt: new Date() 
     }],{
     })
   },
