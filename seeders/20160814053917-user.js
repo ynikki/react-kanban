63e011a307b13f959e2b507bc080f52405eb421c
Nikki K <808nikz@gmail.com>
2016-08-20 23:53:55 -1000

    Added test uers

--- seeders/20160814053917-user.js
+++ seeders/20160814053917-user.js
@@ -4,7 +4,23 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
     {
-      "username" : "blank_something",
+      "username": "blank_something",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      "username": "holy_moly",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      "username": "patty_cake",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      "username": "mew_mew",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      "username": "shambalah",
       createdAt: new Date(),
       updatedAt: new Date()
     }],{
